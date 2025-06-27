404015a419d870753f2a57bd7fdcb871ebf8a9c2
kyden <kydenul@gmail.com>
2025-06-27 15:04:58 +0800

    feat: add gui_background color and timeout

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -62,7 +62,13 @@ require("lazy").setup({
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
+      {
+        "rcarriga/nvim-notify",
+        opts = {
+          background_colour = "#000000",
+          timeout = 2400,
+        },
+      },
     },
     config = function()
       require("config.noice")
