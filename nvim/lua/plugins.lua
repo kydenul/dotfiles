037340584154523836c9eb6d954fa2824824bb26
kyden <kydenul@gmail.com>
2025-09-09 13:17:49 +0800

    feat(nvim): add `IDE-like breadcrumbs, out of the box`

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -346,6 +346,22 @@ require("lazy").setup({
     end,
   },
 
+  -- IDE-like breadcrumbs, out of the box
+  {
+    "Bekaboo/dropbar.nvim",
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+    },
+    config = function()
+      local dropbar_api = require("dropbar.api")
+      vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+      vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+      vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+    end,
+  },
+
   -- Vscode-like pictograms
   {
     "onsails/lspkind.nvim",
