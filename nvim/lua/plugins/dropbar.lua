086994961204548c897f3b17476e0d1ff162c8f3
kyden <kydenul@gmail.com>
2026-04-15 11:37:52 +0800

    🔧 chore(nvim): configure dropbar to exclude non-code windows
    Disable winbar for NvimTree, dashboard, and alpha filetypes to
    prevent breadcrumb navigation from appearing in non-code buffers.

--- nvim/lua/plugins/dropbar.lua
+++ nvim/lua/plugins/dropbar.lua
@@ -10,6 +10,22 @@ return {
   },
 
   config = function()
+    require("dropbar").setup({
+      bar = {
+        enable = function(buf, win, _)
+          local ft = vim.bo[buf].filetype
+          -- Disable winbar for NvimTree and other non-code windows
+          local excluded = { NvimTree = true, dashboard = true, alpha = true }
+          if excluded[ft] then
+            return false
+          end
+          return vim.fn.win_gettype(win) == ""
+            and vim.wo[win].winbar == ""
+            and vim.bo[buf].buftype == ""
+        end,
+      },
+    })
+
     local dropbar_api = require("dropbar.api")
     vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
     vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
